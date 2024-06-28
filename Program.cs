e1bfbb9716ab6c5876cd541ef138453a3aa344f9
VanMachado <vandamme.machado@gmail.com>
2024-06-28 16:19:59 -0300

    Primeiro commit: Adicionando projeto Fibonacci

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int anterior = 1;
+int atual = 1;
+List<int> fibonacci = new List<int>();
+fibonacci.Add(anterior);
+fibonacci.Add(atual);
+
+for (int i =0; i <5; i++)
+{
+    int futuro = anterior + atual;
+    anterior = atual;
+    atual = futuro;
+    
+    fibonacci.Add(futuro);    
+}
